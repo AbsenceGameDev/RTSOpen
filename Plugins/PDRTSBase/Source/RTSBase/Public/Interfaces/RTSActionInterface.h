e292f62b14c51d0b73b49ff161da809141fa7883
Ario Amin <ario.amin@permadev.se>
2024-04-10 04:12:27 +0200

    Initial commit, adding base source files and some empty interfaces for now

---
+++ Plugins/PDRTSBase/Source/RTSBase/Public/Interfaces/RTSActionInterface.h
@@ -0,0 +1,69 @@
+/* @author: Ario Amin @ Permafrost Development. @copyright: Full MIT License included at bottom of the file  */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "RTSActionInterface.generated.h"
+
+UENUM()
+enum class ERTSActionMode : uint8
+{
+	Build,
+	Harvest,
+};
+
+UINTERFACE(MinimalAPI)
+class URTSActionInterface : public UInterface { GENERATED_BODY() };
+
+/**
+ * @brief This interface will be placed on pawns or characters we want to perform an action from.
+ * @note It is abstracted in the plugin to allow anything else to hook into it in the game layer
+ */
+class PDRTSBASE_API IRTSActionInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, CallInEditor, Category = "Action|Interface")
+	void StartAction(ERTSActionMode ActionMode, int32 ActionID) const;
+	virtual void StartAction_Implementation(ERTSActionMode ActionMode, int32 ActionID) const
+	{
+		// Perform actions
+		return;
+	}
+
+	UFUNCTION(BlueprintNativeEvent, CallInEditor, Category = "Action|Interface")
+	void StopAction(ERTSActionMode ActionMode, int32 ActionID) const;
+	virtual void StopAction_Implementation(ERTSActionMode ActionMode, int32 ActionID) const
+	{
+		// Perform actions
+		return;
+	}	
+public:
+};
+
+
+
+/*
+ * @copyright Permafrost Development (MIT license) 
+ * Authors: Ario Amin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
---
+++ Plugins/PDRTSBase/Source/RTSBase/Public/Interfaces/RTSActorInterface.h
@@ -0,0 +1,55 @@
+/* @author: Ario Amin @ Permafrost Development. @copyright: Full MIT License included at bottom of the file  */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "RTSActorInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class URTSActorInterface : public UInterface { GENERATED_BODY() };
+
+/**
+ * @brief This interface will be placed on actors we want to perform an action in.
+ * @note Allows us to return an ID  will be associated with the actor
+ */
+class PDRTSBASE_API IRTSActorInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, CallInEditor, Category = "Actor|Interface")
+	void GetActorActionID() const;
+	virtual int32 GetActorActionID_Implementation() const
+	{
+		// Perform actions
+		return INDEX_NONE;
+	}
+public:
+};
+
+
+
+
+/*
+ * @copyright Permafrost Development (MIT license) 
+ * Authors: Ario Amin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
